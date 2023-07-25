90d52706ff7361b575288269651b754990eeb473
meenachinmay <Chinmayanand896@gmail.com>
2023-07-25 15:20:09 +0900

    NERDTree added and keymappings added for screen spliting and moving the window

--- lua/meenachinmay/remap.lua
+++ lua/meenachinmay/remap.lua
@@ -4,9 +4,18 @@ local map = vim.api.nvim_set_keymap
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n","<leader>:q", ':q<cr>')
 
+-- split window
 vim.keymap.set('n','ss', ':split<Return><C-w>w')
 vim.keymap.set('n','sv', ':vsplit<Return><C-w>w')
 
+-- Move window
+vim.keymap.set('n', 'sh', '<C-w>h')
+vim.keymap.set('n', 'sk', '<C-w>k')
+vim.keymap.set('n', 'sj', '<C-w>j')
+vim.keymap.set('n', 'sl', '<C-w>l')
+
+-- Select ALL
+vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 map('n', '<leader>e', ':NERDTreeToggle<cr>', {noremap = true, silent = false})
 map('n', '<leader>q', ':q<cr>', {noremap = true, silent = false})
